d172da8fd0cac77218d576334d04870fdb0a53c2
王伟 <ERVeepp@users.noreply.github.com>
2021-10-14 15:21:33 +0800

    代码微调
    2021年10月14日15:19:50
    王伟

--- src/router/modules/demo.ts
+++ src/router/modules/demo.ts
@@ -1,40 +1,158 @@
 import { RouterTy } from '@/types/router'
 import Layout from '@/layout'
-export default {
-  path: '/writing-demo',
-  component: Layout,
-  meta: { title: 'Writing Demo', icon: 'eye-open' },
-  alwaysShow: true,
-  children: [
-    {
-      path: 'hook',
-      component: () => import('@/views/example/hook/Hook.vue'),
-      name: 'Hook',
-      meta: { title: 'Hook-Demo' }
+export default [
+  {
+    path: '/example',
+    component: Layout,
+    redirect: '/example/table',
+    name: 'Example',
+    meta: { title: 'Example', icon: 'example' },
+    children: [
+      {
+        path: 'table',
+        name: 'Table',
+        component: () => import('@/views/table/index'),
+        meta: { title: 'Table', icon: 'table' }
+      },
+      {
+        path: 'tree',
+        name: 'Tree',
+        component: () => import('@/views/tree/index'),
+        meta: { title: 'Tree', icon: 'tree' }
+      }
+    ]
+  },
+
+  {
+    path: '/form',
+    component: Layout,
+    children: [
+      {
+        path: 'index',
+        name: 'Form',
+        component: () => import('@/views/form/index'),
+        meta: { title: 'Form', icon: 'table' }
+      }
+    ]
+  },
+
+  {
+    path: '/nested',
+    component: Layout,
+    redirect: '/nested/menu1',
+    name: 'Nested',
+    meta: {
+      title: 'Nested',
+      icon: 'nested'
     },
-    {
-      path: 'vuex-use',
-      component: () => import('@/views/example/vuex-use/VuexUse.vue'),
-      name: 'VuexUse',
-      meta: { title: 'Vuex-Demo' }
-    },
-    {
-      path: 'mock-test',
-      component: () => import('@/views/example/mock-test/MockTest.vue'),
-      name: 'MockTest',
-      meta: { title: 'Mock-Demo' }
-    },
-    {
-      path: 'svg-icon',
-      component: () => import('@/views/example/svg-icon/SvgIcon.vue'),
-      name: 'SvgIcon',
-      meta: { title: 'Svg-Demo' }
-    },
-    {
-      path: 'parent-children',
-      component: () => import('@/views/example/parent-children/Parent.vue'),
-      name: 'Parent',
-      meta: { title: 'Parent-Children' }
-    }
-  ]
-}
+    children: [
+      {
+        path: 'menu1',
+        component: () => import('@/views/nested/menu1/index'), // Parent router-view
+        name: 'Menu1',
+        meta: { title: 'Menu1' },
+        children: [
+          {
+            path: 'menu1-1',
+            component: () => import('@/views/nested/menu1/menu1-1'),
+            name: 'Menu1-1',
+            meta: { title: 'Menu1-1' }
+          },
+          {
+            path: 'menu1-2',
+            component: () => import('@/views/nested/menu1/menu1-2'),
+            name: 'Menu1-2',
+            meta: { title: 'Menu1-2' },
+            children: [
+              {
+                path: 'menu1-2-1',
+                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
+                name: 'Menu1-2-1',
+                meta: { title: 'Menu1-2-1' }
+              },
+              {
+                path: 'menu1-2-2',
+                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
+                name: 'Menu1-2-2',
+                meta: { title: 'Menu1-2-2' }
+              }
+            ]
+          },
+          {
+            path: 'menu1-3',
+            component: () => import('@/views/nested/menu1/menu1-3'),
+            name: 'Menu1-3',
+            meta: { title: 'Menu1-3' }
+          }
+        ]
+      },
+      {
+        path: 'menu2',
+        component: () => import('@/views/nested/menu2/index'),
+        name: 'Menu2',
+        meta: { title: 'menu2' }
+      }
+    ]
+  },
+  {
+    path: '/external-link',
+    component: Layout,
+    children: [
+      {
+        component: () => {},
+        path: 'https://github.com/jzfai/vue3-admin-ts.git',
+        meta: { title: 'External Link', icon: 'link' }
+      }
+    ]
+  },
+  {
+    path: '/error-log',
+    component: Layout,
+    children: [
+      {
+        path: 'log',
+        component: () => import('@/views/error-log/index'),
+        name: 'ErrorLog',
+        meta: { title: 'Error Log', icon: 'bug' }
+      }
+    ]
+  },
+  {
+    path: '/writing-demo',
+    component: Layout,
+    meta: { title: 'Writing Demo', icon: 'eye-open' },
+    alwaysShow: true,
+    children: [
+      {
+        path: 'hook',
+        component: () => import('@/views/example/hook/Hook.vue'),
+        name: 'Hook',
+        meta: { title: 'Hook-Demo' }
+      },
+      {
+        path: 'vuex-use',
+        component: () => import('@/views/example/vuex-use/VuexUse.vue'),
+        name: 'VuexUse',
+        meta: { title: 'Vuex-Demo' }
+      },
+      {
+        path: 'mock-test',
+        component: () => import('@/views/example/mock-test/MockTest.vue'),
+        name: 'MockTest',
+        meta: { title: 'Mock-Demo' }
+      },
+      {
+        path: 'svg-icon',
+        component: () => import('@/views/example/svg-icon/SvgIcon.vue'),
+        name: 'SvgIcon',
+        meta: { title: 'Svg-Demo' }
+      },
+      {
+        path: 'parent-children',
+        component: () => import('@/views/example/parent-children/Parent.vue'),
+        name: 'Parent',
+        meta: { title: 'Parent-Children' }
+      }
+    ]
+  }
+]
