a26b01ff32dc1f45e2545796ad428c7b9f259e08
王伟 <ERVeepp@users.noreply.github.com>
2021-10-18 10:47:06 +0800

    add dist
    2021年10月18日10:47:04
    王伟

---
+++ dist/assets/index.77b1d549.js
@@ -0,0 +1,1 @@
+import{d as a,J as e,L as r,e as s}from"./vendor.ab9e672f.js";var t=a({setup(){const a=e(),t=r();return s((()=>{const{params:e,query:r}=a,{path:s}=e;t.replace({path:"/"+s,query:r})})),()=>""}});export{t as default};
