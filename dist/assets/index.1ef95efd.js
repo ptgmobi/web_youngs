2920154ab5f023a7cccf950dcbe1f3bd557c12fe
ERVee Wang(王伟) <ervee.wang@yeahmobi.com>
2021-10-21 13:22:42 +0800

    build
    2021-10-21 13:22:40
    王伟

---
+++ dist/assets/index.1ef95efd.js
@@ -0,0 +1,1 @@
+import{d as a,J as e,L as r,e as s}from"./vendor.7dbf92ed.js";var t=a({setup(){const a=e(),t=r();return s((()=>{const{params:e,query:r}=a,{path:s}=e;t.replace({path:"/"+s,query:r})})),()=>""}});export{t as default};
