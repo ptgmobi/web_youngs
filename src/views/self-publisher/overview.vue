db4c5240ca3175c75829d7244a9c8b1c249c8e92
ERVee Wang(王伟) <ervee.wang@yeahmobi.com>
2021-10-26 17:45:26 +0800

    test
    2021-10-26 17:45:24
    王伟

--- src/views/self-publisher/overview.vue
+++ src/views/self-publisher/overview.vue
@@ -1,66 +1,64 @@
 <template>
   <div>
-    <ww-chart-data class="mt-40" :chart-data="chartData" />
+    <ww-chart-data class="mt-40" :chart-data="data.chartData" />
+    <DD :count="data.count" @kk="handleClick" />
   </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
+import { reactive } from 'vue'
 import WwChartData from '@/components/Self/WwChart/WwChartData'
-import store from '@/store'
-console.log(store.state.user.email)
-export default {
-  components: { WwChartData },
-  data() {
-    return {
-      chartData: {
-        tooltip: {
-          trigger: 'axis',
-          axisPointer: {
-            type: 'cross',
-            label: {
-              show: true,
-              backgroundColor: '#6a7985'
-            }
-          }
-        },
-        legend: {
-          show: false,
-          data: ['全部渠道']
-        },
-        grid: {
-          left: '3%',
-          right: '4%',
-          bottom: '3%',
-          containLabel: true
-        },
-        xAxis: [
-          {
-            type: 'category',
-            boundaryGap: false,
-            data: [null]
-          }
-        ],
-        yAxis: [
-          {
-            type: 'value'
-          }
-        ],
-        series: [
-          {
-            name: '全部渠道',
-            type: 'line',
-            emphasis: {
-              focus: 'series'
-            },
-            data: [0, 0, 0, 0, 0, 0, 0, 0]
-          }
-        ]
+import DD from './test'
+
+const data = reactive({
+  count: 666,
+  chartData: {
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'cross',
+        label: {
+          show: true,
+          backgroundColor: '#6a7985'
+        }
       }
-    }
-  },
-  created() {
-    console.log(this.$store.state.user.email)
+    },
+    legend: {
+      show: false,
+      data: ['全部渠道']
+    },
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      containLabel: true
+    },
+    xAxis: [
+      {
+        type: 'category',
+        boundaryGap: false,
+        data: [null]
+      }
+    ],
+    yAxis: [
+      {
+        type: 'value'
+      }
+    ],
+    series: [
+      {
+        name: '全部渠道',
+        type: 'line',
+        emphasis: {
+          focus: 'series'
+        },
+        data: [0, 0, 0, 0, 0, 0, 0, 0]
+      }
+    ]
   }
+})
+const handleClick = (data: string) => {
+  console.log(data)
 }
 </script>
 
--- src/views/self-publisher/test.vue
+++ src/views/self-publisher/test.vue
@@ -1,14 +1,22 @@
 <template>
   <div>
-    test
+    <span @click="handleClick">test</span>
+    {{ props.count }}
   </div>
 </template>
 <script setup lang="ts">
-import * as data from 'vue'
+import { defineProps, defineEmits, nextTick } from 'vue'
 import { useStore } from 'vuex'
-console.log(data)
 import $store from '@/store'
+await nextTick()
 console.log($store.state.user.email)
 const store = useStore()
 console.log(store.state.user.email)
+const props = defineProps({
+  count: Number
+})
+const emit = defineEmits(['kk', 'up'])
+const handleClick = () => {
+  emit('kk', '点了我')
+}
 </script>
