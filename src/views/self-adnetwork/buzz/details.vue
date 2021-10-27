972f6c0382f44ac4b501b288f5489269a1749e98
ERVee Wang(王伟) <ervee.wang@yeahmobi.com>
2021-10-27 18:31:00 +0800

    功能开发
    2021-10-27 18:30:58
    王伟

--- src/views/self-adnetwork/buzz/details.vue
+++ src/views/self-adnetwork/buzz/details.vue
@@ -1,10 +1,427 @@
 <template>
   {{router.currentRoute.value.params}}
+  {{router.currentRoute.value.query}}
+  <!-- form -->
+  <el-form enctype="multipart/form-data" ref='data.ruleForm' :rules='data.rules' :model='data.ruleForm' label-width="240px" label-position="right">
+    <div class='content-con flex column'>
+      <!-- offer_id -->
+      <el-form-item label="Offer ID:" prop="offer_id" v-if='data.type === "2"'>
+        <div class='flex flex-start form-one radio-box'>
+          <span v-text='data.ruleForm.offer_id'></span>
+        </div>
+      </el-form-item>
+      <!-- channel -->
+      <el-form-item label="Channel:" prop="channel">
+        <el-select filterable class='form-one' v-model="data.ruleForm.channel" clearable placeholder="">
+          <el-option
+            v-for="item in data.options.channel"
+            :key="item"
+            :label="item"
+            :value="item">
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <!-- copy_offer -->
+      <el-form-item label="Copy Offer:" prop="copy_offer">
+        <el-input class='form-one copy-btn search-input' placeholder="" v-model.trim="data.ruleForm.copy_offer">
+          <el-button type="primary" slot="append" icon="el-icon-search" @click='copyFun'></el-button>
+        </el-input>
+      </el-form-item>
+      <!-- attribute_provider -->
+      <el-form-item label="Attribute Provider:" prop="attribute_provider">
+        <el-select filterable class='form-one' v-model="data.ruleForm.attribute_provider" clearable placeholder="">
+          <el-option
+            v-for="item in data.options.attribute_provider"
+            :key="item"
+            :label="item"
+            :value="item">
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <!-- title -->
+      <el-form-item label="Offer Title:" prop="title">
+        <el-input class='form-one' type='text' v-model.trim="data.ruleForm.title" placeholder=''></el-input>
+      </el-form-item>
+      <!-- tracking_link -->
+      <!-- 此处需要去除空格和制表符 -->
+      <el-form-item label="Traking Link:" prop="tracking_link">
+        <el-input class='form-one' type="textarea" v-model.trim="data.ruleForm.tracking_link" 
+        :autosize="{ minRows: 8, maxRows: 80}"
+        placeholder=''></el-input>
+      </el-form-item>
+      <!-- pkg_name -->
+      <el-form-item label="Package Name:" prop="pkg_name">
+        <el-input class='form-one' type='text' v-model.trim="data.ruleForm.pkg_name" placeholder=''></el-input>
+      </el-form-item>
+      <!-- conversion_flow_type -->
+      <el-form-item label="Conversion Flow:" prop="conversion_flow">
+        <div class='form-one flex flex-start'>
+          <el-radio v-model="data.ruleForm.conversion_flow" label="1">CPI</el-radio>
+          <el-radio v-model="data.ruleForm.conversion_flow" label="2">CPA</el-radio>
+        </div>
+      </el-form-item>
+      <!-- conversion_flow -->
+      <el-form-item label="Event Name:" prop="event_name" v-if="data.ruleForm.conversion_flow === '2'">
+        <el-input class='form-one' type='text' v-model.trim="data.ruleForm.event_name" placeholder=''></el-input>
+      </el-form-item>
+      <!-- payout -->
+      <el-form-item label="Payout($):" prop="payout">
+        <el-input class='form-one' type='text' v-model.trim="data.ruleForm.payout" placeholder=''></el-input>
+      </el-form-item>
+      <!-- platform -->
+      <el-form-item label="Select Platform:" prop="platform">
+        <el-select filterable class='form-one' v-model="data.ruleForm.platform" clearable placeholder="">
+          <el-option
+            v-for="item in data.options.platform"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <!-- country -->
+      <el-form-item label="Targeting Countries:" prop="country">
+        <el-select filterable class='form-one' v-model="data.ruleForm.country" clearable placeholder="">
+          <el-option
+            v-for="item in data.options.country"
+            :key="item"
+            :label="item"
+            :value="item">
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <!-- max_clk_num -->
+      <el-form-item label="max Click Number:" prop="max_clk_num">
+        <el-input class='form-one' type='text' v-model.trim="data.ruleForm.max_clk_num" placeholder=''></el-input>
+      </el-form-item>
+      <!-- Site Click Limitation -->
+      <el-form-item label="Site Click Limitation:" prop="site_clk_limit">
+        <el-input class='form-one' type='text' v-model.trim="data.ruleForm.site_clk_limit" placeholder=''></el-input>
+      </el-form-item>
+      <!-- start_hour -->
+      <el-form-item label="Start Hour:" prop="start_hour">
+        <el-select filterable class='form-one' v-model="data.ruleForm.start_hour" placeholder="">
+          <el-option
+            v-for="item in data.options.time"
+            :key="item"
+            :label="item"
+            :value="item">
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <!-- end_hour -->
+      <el-form-item label="End Hour:" prop="end_hour">
+        <el-select filterable class='form-one' v-model="data.ruleForm.end_hour" placeholder="">
+          <el-option
+            v-for="item in data.options.time"
+            :key="item"
+            :label="item"
+            :value="item">
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <!-- site_install_limitation-->
+      <el-form-item label="Site Install Limitation:" prop="site_install_limitation">
+        <el-select filterable class='form-one' v-model="data.ruleForm.site_install_limitation" clearable placeholder="">
+          <el-option
+            v-for="item in 6"
+            :key="item"
+            :label="item - 1"
+            :value="item - 1">
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <!-- device -->
+      <el-form-item label="Select Device:" prop="device">
+        <div class='flex flex-start form-one'>
+          <span class='cp icon mr-10' @click='editDeviceFun(data.ruleForm.device)'><i class="el-icon-edit-outline"></i></span>
+          <span v-text='data.search.deviceData.count'></span>
+        </div>
+      </el-form-item>
+      <!-- diy_siteid -->
+      <el-form-item label="Diy SiteID:" prop="site">
+        <div class='flex flex-start form-one'>
+          <span class='cp icon mr-10' @click='editDiySiteFun(data.ruleForm)'><i class="el-icon-picture-outline-round"></i></span>
+        </div>
+      </el-form-item>
+      <!-- site_id -->
+      <el-form-item label="Site ID:" prop="site_id">
+        <div class='flex form-one flex-start radio-box'>
+          <el-radio v-model="data.ruleForm.site_id" label="1">Slot_ID</el-radio>
+          <el-radio v-model="data.ruleForm.site_id" label="2">Update(Hours)</el-radio>
+          <el-select filterable class='form-one ml-10' v-model="data.ruleForm.hour" clearable placeholder=""
+          :disabled = "data.ruleForm.site_id !== '2'"
+          >
+            <el-option
+              v-for="item in data.options.site_id"
+              :key="item"
+              :label="item"
+              :value="item">
+            </el-option>
+          </el-select>
+        </div>
+      </el-form-item>
+      <!-- clk_id -->
+      <el-form-item label="Click ID:" prop="clk_id">
+        <div class='flex flex-start form-one flex-start radio-box'>
+          <el-radio v-model="data.ruleForm.clk_id" label="1">Real</el-radio>
+          <el-radio v-model="data.ruleForm.clk_id" label="2">Virtual</el-radio>
+        </div>
+      </el-form-item>
+      <!-- site_clk_id -->
+      <!-- <el-form-item label="Site Click ID:" prop="site_clk_id">
+        <div class='flex flex-start form-one flex-start radio-box'>
+          <el-radio v-model="data.ruleForm.site_clk_id" label="1">ON</el-radio>
+          <el-radio v-model="data.ruleForm.site_clk_id" label="2">OFF</el-radio>
+        </div>
+      </el-form-item> -->
+      <!-- category -->
+      <!-- <el-form-item label="Category:" prop="category_id">
+        <el-select filterable class='form-one' v-model="data.ruleForm.category_id" clearable placeholder="">
+          <el-option
+            v-for="(value, key) in handleOptionsCategory"
+            :key="key"
+            :label="value"
+            :value="key">
+          </el-option>
+        </el-select>
+      </el-form-item> -->
+      <!-- note -->
+      <el-form-item label="Note:" prop="note">
+        <el-input class='form-one' type="textarea" v-model.trim="data.ruleForm.note" 
+        :autosize="{ minRows: 8, maxRows: 80}"
+        placeholder=''></el-input>
+      </el-form-item>
+    </div>
+  </el-form>
+  <!-- form -->
+  <!-- footer -->
+  <div class='w100 flex'>
+    <el-button type="primary" @click='saveFun'>Save</el-button>
+  </div>
+  <!-- footer -->
 </template>
 <script lang="ts" setup>
-import { getCurrentInstance } from 'vue'
+import { getCurrentInstance, reactive } from 'vue'
 let { proxy }: any = getCurrentInstance()
 import {useRouter } from 'vue-router'
 const router = useRouter()
 console.log(router.currentRoute.value)
+const message = {
+  required: '此项必填'
+}
+let validatorSpace = (rule, value, callback) => {
+  let reg = new RegExp('\\s+', 'g')
+  if (reg.test(value)) {
+    callback(new Error('链接中有空格'))
+  } else {
+    callback()
+  }
+  callback()
+}
+let validatorDevice = (rule, value, callback) => {
+  console.log(value)
+  callback()
+}
+let validatorSite = (rule, value, callback) => {
+  if (value) {
+    if (value === '1') {
+      callback()
+    } else {
+      if (data.ruleForm.hour) {
+        callback()
+      } else {
+        callback(new Error('请选择具体数值'))
+      }
+    }
+  } else {
+    callback(new Error(message.required))
+  }
+  
+}
+let validatorConversionFlow = (rule, value, callback) => {
+  if (data.ruleForm.conversion_flow === '1') {
+    callback()
+  } else {
+    if (value) {
+      callback()
+    } else {
+      callback(new Error(message.required))
+    }
+  }
+}
+let validatorStartHour = (rule, value, callback) => {
+  if (value !== '') {
+    if (data.ruleForm.end_hour === '') {
+      callback(new Error(message.required))
+    } else {
+      callback()
+    }
+  }
+}
+let validatorEndHour = (rule, value, callback) => {
+  if (value !== '') {
+    if (data.ruleForm.start_hour === '') {
+      callback(new Error(message.required))
+    } else {
+      callback()
+    }
+  }
+}
+let validateSiteExcel = (rule, value, callback) => {
+  if (value !== '') {
+    const flag = data.analysisExcelFn(value)
+    console.log(flag)
+    if (flag) {
+      callback()
+    } else {
+      callback(new Error('格式错误！'))
+    }
+  } else {
+    callback(new Error(message.required))
+  }
+}
+let data = reactive({
+  cache: {},
+  dialogVisible: false,
+  dialogVisibleSite: false,
+  search: {
+    id: '',
+    baseDeviceSelect: {},
+    deviceData: {
+      all: [],
+      select: [],
+      count: 0,
+    }
+  },
+  type: '1',
+  params: '',
+  loading: true,
+  ssp_security_param: '<?= Yii::$app->request->csrfToken ?>',
+  options: {
+    channel: [],
+    attribute_provider: [
+      'AppsFlyer',
+      'Branch',
+      'Adjust',
+      'Singular',
+      'AppMetrica'
+    ],
+    time: [-1, ...[...new Array(24)].map((ele, index) => index)],
+    platform: [
+      {value: '1', label: 'Android'},
+      {value: '2', label: 'iOS'},
+    ],
+    country: [],
+    site_id: [
+      0.2, 0.4, 0.6, 1, 2, 4, 8, 24, 72, 168
+    ],
+    category: [],
+    devices: []
+  },
+  ruleForm: {
+    id: '',
+    operation_type: '',
+    offer_id: '',
+    channel: '',
+    copy_offer: '',
+    attribute_provider: 'AppsFlyer',
+    title: '',
+    // 过滤空格，制表符
+    tracking_link: '',
+    pkg_name: '',
+    conversion_flow: '',
+    event_name: '',
+    payout: '',
+    platform: '',
+    country: '',
+    max_clk_num: '',
+    site_clk_limit: '0',
+    site_install_limitation: '0',
+    start_hour: -1,
+    end_hour: -1,
+    device: [],
+    diy_siteid: [],
+    site_id: '',
+    hour: '',
+    clk_id: '',
+    site_clk_id: '',
+    category_id: '',
+    note: ''
+  },
+  rules: {
+    channel: [
+      { required: true, message: message.required, trigger: ['blur', 'change'] }
+    ],
+    attribute_provider: [
+      { required: true, message: message.required, trigger: ['blur', 'change'] }
+    ],
+    title: [
+      { required: true, message: message.required, trigger: ['blur', 'change'] }
+    ],
+    tracking_link: [
+      { required: true, message: message.required, trigger: ['blur', 'change'] },
+      { validator: validatorSpace }
+    ],
+    pkg_name: [
+      { required: true, message: message.required, trigger: ['blur', 'change'] }
+    ],
+    conversion_flow: [
+      { required: true, message: message.required, trigger: ['blur', 'change'] }
+    ],
+    event_name: [
+      { required: true, message: message.required, trigger: ['blur', 'change'] },
+      { validator: validatorConversionFlow }
+    ],
+    payout: [
+      { required: true, message: message.required, trigger: ['blur', 'change'] }
+    ],
+    platform: [
+      { required: true, message: message.required, trigger: ['blur', 'change'] }
+    ],
+    country: [
+      { required: true, message: message.required, trigger: ['blur', 'change'] }
+    ],
+    max_clk_num: [
+      { required: true, message: message.required, trigger: ['blur', 'change'] }
+    ],
+    device: [
+      { required: true, validator: validatorDevice, trigger: ['blur', 'change'] }
+    ],
+    site_id: [
+      { required: true, validator: validatorSite, trigger: ['blur', 'change'] }
+    ],
+    clk_id: [
+      { required: true, message: message.required, trigger: ['blur', 'change'] }
+    ],
+    start_hour: [
+      { required: false, validator: validatorStartHour, trigger: ['blur', 'change'] }
+    ],
+    end_hour: [
+      { required: false, validator: validatorEndHour, trigger: ['blur', 'change'] }
+    ]
+    // site_clk_id: [
+    //   { required: true, message: message.required, trigger: ['blur', 'change'] }
+    // ],
+    // category_id: [
+    //   { required: true, message: message.required, trigger: ['blur', 'change'] }
+    // ],
+  },
+  // site
+  siteData: [],
+  siteRules: {
+    site: [
+      { required: true, validator: validateSiteExcel, trigger: 'blur' }
+    ]
+  },
+  siteRuleForm: {
+    site: ''
+  }
+})
+
+// methods
+const copyFun = () => {}
+const editDeviceFun = () => {}
+const editDiySiteFun = () => {}
+const saveFun = () => {}
 </script>
