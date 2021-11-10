f0fbf355902a8515eb8f2fecab17a1a46fe4130d
ERVee Wang(王伟) <ervee.wang@yeahmobi.com>
2021-11-10 15:16:13 +0800

    build
    2021-11-10 15:16:10
    王伟

--- dist/assets/station.3ed6eef9.js
+++
@@ -1,1 +0,0 @@
-import{k as t}from"./index.783b7d78.js";function o(){return t({url:"/positions",method:"get"})}function n(o){return t({url:`/positions/${o}`,method:"get"})}function s(o){return t({url:"/positions",method:"post",data:o})}function r(o,n){return t({url:`/positions/${o}`,method:"patch",data:n})}export{n as a,r as b,o as g,s};
