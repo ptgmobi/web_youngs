6a56881ca1a88c27fa48c8146a37b7fd3a500245
ERVee Wang(王伟) <ervee.wang@yeahmobi.com>
2021-12-07 16:24:23 +0800

    build
    2021-12-07 16:24:21
    王伟

---
+++ dist/assets/index-legacy.737b326e.js
@@ -0,0 +1,1 @@
+System.register(["./vendor-legacy.d18a23a8.js","./index-legacy.ecedc589.js"],(function(e){"use strict";var n,t,r;return{setters:[function(e){n=e.h,t=e.o},function(e){r=e._}],execute:function(){e("default",r({},[["render",function(e,r){return t(),n("div",null," 我是首页 ")}]]))}}}));
---
+++ dist/assets/index.aae7bfe1.js
@@ -0,0 +1,1 @@
+import{h as r,o as a}from"./vendor.abf78dba.js";import{_ as o}from"./index.df9a4cbc.js";var d=o({},[["render",function(o,d){return a(),r("div",null," 我是首页 ")}]]);export{d as default};
