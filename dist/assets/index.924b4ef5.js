f2d1451c3cd24882dac24a2df2e418a0f0de3913
ERVee Wang(王伟) <ervee.wang@yeahmobi.com>
2021-12-07 15:00:06 +0800

    build
    2021-12-07 15:00:03
    王伟

---
+++ dist/assets/index.924b4ef5.js
@@ -0,0 +1,1 @@
+import{h as r,o as a}from"./vendor.abf78dba.js";import{_ as o}from"./index.af8115b7.js";var e=o({},[["render",function(o,e){return a(),r("div",null," 我是首页 ")}]]);export{e as default};
