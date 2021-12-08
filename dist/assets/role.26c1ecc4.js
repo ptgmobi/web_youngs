4f42da232b4a75d50dee84ebfbce6d91834bbf63
ERVee Wang(王伟) <ervee.wang@yeahmobi.com>
2021-12-08 11:29:36 +0800

    build
    2021-12-08 11:29:33
    王伟

--- dist/assets/role.26c1ecc4.js
+++
@@ -1,1 +0,0 @@
-import{k as r}from"./index.beb97a98.js";function t(){return r({url:"/role",method:"get",isParams:!0})}function e(t){return r({url:`/role/view/${t}`,method:"get",bfLoading:!1,isParams:!0})}function a(t){return r({url:"/role",method:"post",data:t})}function o(t,e){return r({url:`/role/${t}`,method:"patch",data:e})}export{e as a,o as b,t as g,a as s};
