92755bd45f4b451acfde0e02fff11102269675a7
ERVee Wang(王伟) <ervee.wang@yeahmobi.com>
2022-01-21 14:45:55 +0800

    样式再调
    2022年01月21日14:45:51
    王伟

--- src/views/self-buzz/old-buzz/list.vue
+++ src/views/self-buzz/old-buzz/list.vue
@@ -96,18 +96,18 @@
     <!-- table -->
     <el-table center v-loading="data.loading" :data="data.list" class="w100" height="60vh" border>
       <el-table-column fixed prop="offer_id" label="Offer ID" align="center" width="70"></el-table-column>
-      <el-table-column fixed prop="channel" label="Channel" align="center" width="70"></el-table-column>
-      <el-table-column prop="pkg_name" label="Package Name" align="center"></el-table-column>
-      <el-table-column prop="title" label="Offer Title" width="120" align="center"></el-table-column>
-      <el-table-column prop="attribute_provider" label="Attribute Provider" align="center"></el-table-column>
+      <el-table-column fixed prop="channel" label="Channel" align="center" width="60"></el-table-column>
+      <el-table-column prop="pkg_name" label="Package Name" align="center" width="110"></el-table-column>
+      <el-table-column prop="title" label="Offer Title" align="center"></el-table-column>
+      <el-table-column prop="attribute_provider" label="Attribute Provider" align="center" width="80"></el-table-column>
       <el-table-column prop="pid" label="Pid" align="center" width="90"></el-table-column>
-      <el-table-column prop="platform" label="Platform" align="center" width="70">
+      <el-table-column prop="platform" label="Platform" align="center" width="65">
         <template #default="scope">
           {{ Number(scope.row.platform) === 1 ? 'Android' : 'iOS' }}
         </template>
       </el-table-column>
-      <el-table-column prop="country" label="Country" align="center" width="70"></el-table-column>
-      <el-table-column prop="payout" label="Payout" align="center" width="70"></el-table-column>
+      <el-table-column prop="country" label="Country" align="center" width="57"></el-table-column>
+      <el-table-column prop="payout" label="Payout" align="center" width="55"></el-table-column>
       <el-table-column label="Click Limitation(w)" width="150" align="center">
         <template #default="scope">
           <div class="flex jc-around">
@@ -132,7 +132,7 @@
           </div>
         </template>
       </el-table-column>
-      <el-table-column width="100" label="Operation" align="center">
+      <el-table-column label="Operation" align="center">
         <template #default="scope">
           <div class="flex jc-around">
             <router-link :to="getEditUrl(scope.row)">
