fc364cfefde23ce66c25d51bb0bb2cb5e040ddc4
ERVee Wang(王伟) <ervee.wang@yeahmobi.com>
2022-02-14 10:42:34 +0800

    build
    2022-02-14 10:42:32
    王伟

--- dist/static/js/power-af91a357.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./index-a9586341.js";function t(){return e({url:"/permission",method:"get",isParams:!0})}function n(s){return e({url:`/permission/view/${s}`,method:"get",isParams:!0})}function o(s){return e({url:"/permission",method:"post",data:s})}function a(s,r){return e({url:"/permission/update",method:"patch",data:r})}export{n as a,a as b,t as g,o as s};
