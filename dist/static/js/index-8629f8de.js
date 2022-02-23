70e8fad60791b76123c7756ba4517880c121dc96
ERVee Wang(王伟) <ervee.wang@yeahmobi.com>
2022-02-23 18:43:37 +0800

    build
    2022年02月23日18:43:26
    王伟

--- dist/static/js/index-8629f8de.js
+++
@@ -1,1 +0,0 @@
-import{r as t,c as e,h as c,o,H as s}from"./vendor-f1931894.js";import{_ as a}from"./index-4d69c6e7.js";const _={};function i(f,m){const r=t("router-view");return o(),e(r,null,{default:c(({Component:n})=>[(o(),e(s(n)))]),_:1})}var p=a(_,[["render",i]]);export{p as default};
---
+++ dist/static/js/station-d847caff.js
@@ -0,0 +1,1 @@
+import{a as o}from"./index-0d058172.js";function e(){return o({url:"/position",method:"get"})}function r(t){return o({url:`/position/view/${t}`,method:"get"})}function s(t){return o({url:"/position",method:"post",data:t})}function a(t,i){return o({url:`/position/${t}`,method:"patch",data:i})}export{r as a,a as b,e as g,s};
