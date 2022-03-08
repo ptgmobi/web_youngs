c8da011fc73e97576cf326194aea1a6f6263a199
ERVee Wang(王伟) <ervee.wang@yeahmobi.com>
2022-03-08 18:42:15 +0800

    删除 vuex-persistedstate,包已弃用
    2022-03-08 18:42:04
    王伟

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,6 @@
 import { createStore } from 'vuex'
 import getters from './getters'
 import { ObjTy } from '@/types/common'
-import createPersistedState from "vuex-persistedstate";
 //auto import (perfect!!!)
 const modulesFiles = import.meta.globEager('./modules/*.ts')
 const modules: ObjTy = {}
@@ -20,18 +19,8 @@ for (const path in modulesFiles) {
 //   return modules
 // }, {})
 // console.log(modules);
-const dataState = createPersistedState({
-  // 默认存储在localStorage 现改为sessionStorage
-  storage: window.sessionStorage,
-  // 本地存储数据的键名
-  key: 'mine',
-  // 指定需要存储的模块，如果是模块下具体的数据需要加上模块名称，如user.token
-  // paths: ['user', 'cart']
-})
+
 export default createStore({
-  plugins: [
-    dataState
-  ],
   modules,
   getters
 })
