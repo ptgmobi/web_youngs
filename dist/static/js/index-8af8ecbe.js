051d4f0d25fd32fc0434f21e5d88ca1fb4374e94
ERVee Wang(王伟) <ervee.wang@yeahmobi.com>
2022-04-25 15:19:55 +0800

    build
    2022-04-25 15:19:53
    王伟

---
+++ dist/static/js/index-8af8ecbe.js
@@ -0,0 +1,1 @@
+import{r,c as e,h as o,o as a,I as n}from"./vendor-ede5cb5d.js";import{_ as s}from"./index-4f17f501.js";var t=s({},[["render",function(s,t){const d=r("router-view");return a(),e(d,null,{default:o((({Component:r})=>[(a(),e(n(r)))])),_:1})}]]);export{t as default};
