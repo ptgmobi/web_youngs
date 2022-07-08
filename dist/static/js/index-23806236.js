b7aff12b5c047620268bb54fb1a3defcba54a9b6
ERVee Wang(王伟) <ervee.wang@yeahmobi.com>
2022-07-08 12:30:05 +0800

    build
    2022年07月08日12:30:03
    王伟

---
+++ dist/static/js/index-23806236.js
@@ -0,0 +1,1 @@
+import{d as s,n as a,L as e,s as r}from"./index-9a016325.js";const t=s({setup(s){let{proxy:t}=e();return t.$router.push({path:"/dashboard/overview"}),(s,e)=>(r(),a("div",null,"Index"))}});export{t as default};
--- dist/static/js/list-336349d4.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./index-951713b3.js";var e=r({},[["render",function(r,e){return"list"}]]);export{e as default};
