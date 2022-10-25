6bce2fc0013144b01104fe6cc56aec63e7cfa239
ERVee Wang(王伟) <ervee.wang@yeahmobi.com>
2022-10-25 16:13:44 +0800

    微调
    2022年10月25日16:13:42
    王伟

--- src/router/dspmodules/advertiser.ts
+++ src/router/dspmodules/advertiser.ts
@@ -6,7 +6,7 @@ export default [
     component: Layout,
     name: 'advertiser',
     meta: {
-      title: 'advertiser',
+      title: 'Advertiser',
       elSvgIcon: 'Setting',
       code: 100000
     },
